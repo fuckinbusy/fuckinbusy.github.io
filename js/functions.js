76a6641d58f1c631e74fe0f735ff63a9d79769ec
richard <40494935+fuckinbusy@users.noreply.github.com>
2024-04-20 20:30:07 +0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
 var tg = window.Telegram.WebApp;
 var p_username = document.getElementById("username");
 var p_is_premium = document.getElementById("is_premium");
+
+var insert_data_button = document.getElementById("insert_data_button");
+var exit_button = document.getElementById("exit_button");
+
 var is_filled = false;
 
-export function on_click() {
+
+function on_click() {
     if (!is_filled) {
         let _user = tg.initDataUnsafe.user;
         let username = _user.username;
@@ -11,5 +16,9 @@ export function on_click() {
 
         p_username += username;
         p_is_premium.textContent += is_premium;
+        is_filled = true;
     }
 }
+
+insert_data_button.addEventListener("click", on_click);
+exit_button.addEventListener("click", tg.close);
