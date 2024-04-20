e2c4032529bcaa20d1db3f3d76328e62067894a6
richard <40494935+fuckinbusy@users.noreply.github.com>
2024-04-20 20:17:46 +0800

    Update events.js

--- js/events.js
+++ js/events.js
@@ -4,5 +4,5 @@ var tg = window.Telegram.WebApp;
 var insert_data_button = document.getElementById("insert_data_button");
 var exit_button = document.getElementById("exit_button");
 
-insert_data_button.addEventListener("click", on_click());
-exit_button.addEventListener("click", tg.close());
+insert_data_button.addEventListener("click", on_click);
+exit_button.addEventListener("click", tg.close);
