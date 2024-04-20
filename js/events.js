bf18966dac31d850d92eff99e138797f6d38a699
richard <40494935+fuckinbusy@users.noreply.github.com>
2024-04-20 20:30:26 +0800

    Delete js/events.js

--- js/events.js
+++
@@ -1,8 +0,0 @@
-import { on_click } from "./functions";
-
-var tg = window.Telegram.WebApp;
-var insert_data_button = document.getElementById("insert_data_button");
-var exit_button = document.getElementById("exit_button");
-
-insert_data_button.addEventListener("click", on_click);
-exit_button.addEventListener("click", tg.close);
