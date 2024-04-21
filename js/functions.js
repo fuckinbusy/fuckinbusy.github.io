e9d7d0b63dd0c375ebd1c893139cd34a1eb61732
richard <vasenev.231@mail.ru>
2024-04-21 12:54:58 +0800

    changed

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,56 @@
-var tg = window.Telegram.WebApp;
-var p_username = document.getElementById("username");
-var p_is_premium = document.getElementById("is_premium");
-var insert_data_button = document.getElementById("insert_data_button");
-var exit_button = document.getElementById("exit_button");
-var img_photo = document.getElementById("photo");
-var error = document.getElementById("error");
-var is_filled = false;
+var tg = window.Telegram.WebApp; // Telegram API object
 
-tg.expand();
+var list_inputs = document.getElementById("inputs").children;
+var input_username = list_inputs[0];
+var input_password = list_inputs[1];
+
+var send_data_button = document.getElementById("send_data_button");
+var exit_button = document.getElementById("exit_button");
+var error = document.getElementById("error");
+
+
+function print_error(text) { 
+    /* Inserts text into #error <p> tag */
+    error.style.display = "inline";
+    error.textContent = text;
+}
+function clear_error() {
+    /* Clears text in #error <p> tag */
+    error.textContent = "";
+    error.style.display = "none";
+}
+
 tg.ready();
-var user = tg.initDataUnsafe.user;
+var user = tg.initDataUnsafe.user; // Telegram WebAppUser
 if (!user) {
     print_error();
     tg.close();
 }
-tg.showAlert('Добро пожаловать, ' + tg.initDataUnsafe.user.username);
+tg.showAlert('Добро пожаловать, ' + tg.initDataUnsafe.user.first_name);
 
-function print_error() {
-    error.textContent = "Error";
-}
-
-insert_data_button.addEventListener("click", () => {
-    if (!is_filled) {
-        let username = user.username;
-        let photo_url = user.photo_url;
-        let is_premium = "No";
-
-        if (user.is_premium == true)
-        {
-            is_premium = "Yes";
-        }
-
-        img_photo.src = photo_url;
-        p_username.textContent += username;
-        p_is_premium.textContent += is_premium;
-        is_filled = true;
+send_data_button.addEventListener("click", () => {
+    /* Click listener for send_data_button */
+    clear_error();
+    let username = input_username.value;
+    let password = input_password.value;
+    if (!username || username == "" && !password || password == "") {
+        print_error("Username and password cannot be empty");
+        return;
     }
+    
+    if (username.length < 2) {
+        print_error("Username is too short");
+        return;
+    }
+
+    if (password.length < 2) {
+        print_error("Password is too short");
+        return;
+    }
+
+    let data = {username: username, password: password}
+    tg.sendData(JSON.stringify(data));
+    tg.close();
 });
 
 exit_button.addEventListener("click", () => {
